747b50d369d547a771550b10a9e5c531fb4061ab
ursala lindsey <ursalalindsey@ursalas-MacBook-Air.local>
2020-10-25 22:03:07 -0400

    Added working function page to hazy-calculator

---
+++ index.js
@@ -0,0 +1,46 @@
+const operators = ['+', '-', '*', '/']
+
+const isStringNumberOrIsANumber = (calcSteps) => {
+  return !isNaN(calcSteps) && calcSteps !== ''
+}
+const isOperand = (calcSteps) => {
+  return operators.includes(calcSteps)
+}
+const calculateUpdatedArray = ([num1, operators, num2]) => {
+  switch (operators) {
+    case '+':
+      return num1 + num2
+    case '*':
+      return num1 * num2
+    case '-':
+      return num1 - num2
+    case '/':
+      return num1 / num2
+
+    default: return NaN
+  }
+}
+const calculate = (calcSteps) => {
+  const newString = []
+
+  for (let i = 0; i < calcSteps.length; i++) {
+    if (isStringNumberOrIsANumber(calcSteps[i])) {
+      newString.push(Number(calcSteps[i]))
+    } else if (isOperand(calcSteps[i])) {
+      newString.push(calcSteps[i])
+    }
+  }
+
+  return calculateUpdatedArray(newString)
+}
+
+module.exports = calculate
+
+
+
+
+
+
+
+
+
